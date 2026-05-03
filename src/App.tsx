198c20361de8242dea82ec27054400bb23a8cf18
Javed Khan <javedalikhan50@gmail.com>
2026-05-04 00:01:22 +0530

    Remove base path for root domain deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
 
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
